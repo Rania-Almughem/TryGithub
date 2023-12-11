40fcbf23531e32eb6e258f294d5aca72a48514a1
rania-almughem <rania.almughem@gmail.com>
2023-12-11 10:41:45 +0300

    Initial Commit

---
+++ TryGithub/TryGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryGithubApp.swift
+//  TryGithub
+//
+//  Created by rania on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TryGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
